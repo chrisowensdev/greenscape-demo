2238284aaf04dcbc57bdf4aea998a863d1bb7937
Christopher Owens <cowensdev@gmail.com>
2025-07-15 19:39:32 -0400

    Initial Commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Products() {
+	return (
+		<>
+			<h1>Products</h1>
+			<h2>Product 1</h2>
+			<h2>Product 2</h2>
+			<h2>Product 3</h2>
+		</>
+	);
+}
