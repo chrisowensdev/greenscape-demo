4f4e8c99adbec1f1598cfb7f7ea2789ca651fb15
Christopher Owens <cowensdev@gmail.com>
2025-11-02 15:58:41 -0500

    Update security

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,29 @@ const nextConfig: NextConfig = {
 	output: "export",
 	images: { unoptimized: true },
 	trailingSlash: true,
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Strict-Transport-Security",
+						value: "max-age=31536000; includeSubDomains",
+					},
+					{ key: "X-Frame-Options", value: "SAMEORIGIN" },
+					{ key: "X-Content-Type-Options", value: "nosniff" },
+					{
+						key: "Referrer-Policy",
+						value: "strict-origin-when-cross-origin",
+					},
+					{
+						key: "Permissions-Policy",
+						value: "geolocation=(), microphone=(), camera=(), usb=()",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
